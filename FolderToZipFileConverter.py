eb5f730f11d2d907f2328055c8c7ad3ba0d85ae1
Yash Jain <yashjain12yj@gmail.com>
2016-11-27 13:47:47 +0530

    Folder to zip converter
    Program takes file path in the input and output zip file of that folder.

---
+++ FolderToZipFileConverter.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+# Open a file
+path = input("Enter the path of the folder : ")
+folder_name=input("Enter the folder name : ")
+os.chdir(path)
+if os.path.isdir(folder_name):
+	#If the selected file is a folder
+	print("Creating...")
+	shutil.make_archive(folder_name, "zip", folder_name)
+	#					(Jis naam se banega, type , jiska banana hai)
+	print("Zip file Created!!")
+else:
+	print(folder_name+" is not a folder, hence it can not be zipped.")
